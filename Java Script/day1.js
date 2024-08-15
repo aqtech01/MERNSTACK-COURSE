cd421426bea25bbdccf5ed48ecacee2e2b8a983f
aqtech <abdulqadeer0120@gmail.com>
2024-08-15 10:50:56 +0100

    da

---
+++ Java Script/day1.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// let name = " ";
+// if (name) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// let stock = 10 / "abc";
+// if (stock) {
+//   console.log("true");
+// } else {
+//   console.log("False");
+// }
+// console.log(stock);
+// console.log(typeof stock);
+
+// let a = "";
+// console.log(Number(a));
+// if (a == true) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+// let stock = 10 / 0;
+// if (stock) {
+//   console.log("true");
+// } else {
+//   console.log("False");
+// }
+// console.log(typeof stock);
+
+// Collections
